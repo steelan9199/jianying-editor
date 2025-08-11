6c980377b7daab129fbfe5b6ae59fd989d7aa4b3
yashujs <1686539697@qq.com>
2025-08-11 18:26:17 +0800

    1

--- managers/config/LyricsTaskInfoManager.js
+++ managers/config/LyricsTaskInfoManager.js
@@ -6,21 +6,11 @@ import { BaseManager } from "../properties/BaseManager.js";
  * @extends {BaseManager<any>} // 使用 any 替代未定义的 Track 类型
  */
 export class LyricsTaskInfoManager extends BaseManager {
-
   /**
    * Creates an instance of LyricsTaskInfoManager.
    * @param {any[]} [initialItems=[]] - An array of initial LyricsTaskInfo items.
    */
   constructor(initialItems = []) {
     super(initialItems);
-  }
-
-  /**
-   * 新增一个 LyricsTaskInfo 对象
-   * @param {object} [itemData={}] - Initial data for the new LyricsTaskInfo object.
-   * @returns {object} The newly created LyricsTaskInfo object.
-   */
-  create(itemData = {}) {
-    return this._create(itemData);
   }
 }
